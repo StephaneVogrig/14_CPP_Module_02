e5890e9d38fed03bf34867d098a33e5794ebe484
Stephane Vogrig <svogrig@3D5.42angouleme.fr>
2025-02-21 14:23:49 +0100

    ex00 clean main.cpp

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 20:46:03 by svogrig           #+#    #+#             */
-/*   Updated: 2025/02/21 01:35:34 by svogrig          ###   ########.fr       */
+/*   Updated: 2025/02/21 14:16:13 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,16 @@ int main(void)
 	Fixed c;
 
 	std::cout << std::endl;
-	
+
 	c = b;
 
 	std::cout << std::endl;
-	
+
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
 
 	std::cout << std::endl;
-	
+
 	return 0;	
 }
