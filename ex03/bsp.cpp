10e601d7c871649e2053cad84c7955ef48bb658b
Stephane Vogrig <svogrig@3C2.42angouleme.fr>
2025-02-23 13:48:59 +0100

    ex03 add comment to bsp, change value for main test

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,15 +6,39 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 20:54:21 by svogrig           #+#    #+#             */
-/*   Updated: 2025/02/23 01:30:08 by svogrig          ###   ########.fr       */
+/*   Updated: 2025/02/23 13:47:39 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
+/*
+	How to determine if a point is in a 2D triangle?
+
+	This routine works by taking each line in the triangle and determining which
+	side of the line the point is on.
+	This is done using the "determinant" of the three points.
+	The side is indicate by the sign of the determinant.
+	If determinant is zero, the point is on the line.
+	If a point is on the same side (same determinant sign) of all sides in the triangle,
+	the point is inside the triangle.
+	Conversely, if a point isn't on the same side, it is out of side the triangle.
+	This applies no matter the order in which the line are presented,
+	as long as the points are traversed in order.
+
+	https://stackoverflow.com/questions/2049582/how-to-determine-if-a-point-is-in-a-2d-triangle
+
+	More methode and accuracy problems:
+	https://totologic.blogspot.com/2014/01/accurate-point-in-triangle-test.html
+
+	Implemantation in many langages:
+	https://rosettacode.org/wiki/Find_if_a_point_is_within_a_triangle#C
+
+*/
+
 Fixed determinant(Point const p1, Point const p2, Point const p3)
 {
-	return (p1.getX() - p3.getX()) * (p2.getY() - p3.getY()) - (p2.getX() - p3.getX()) * (p1.getY() - p3.getY());
+	return ( (p1.getX() - p3.getX()) * (p2.getY() - p3.getY()) - (p2.getX() - p3.getX()) * (p1.getY() - p3.getY()) );
 }
 
 bool bsp( Point const a, Point const b, Point const c, Point const point)
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 20:46:03 by svogrig           #+#    #+#             */
-/*   Updated: 2025/02/23 01:31:13 by svogrig          ###   ########.fr       */
+/*   Updated: 2025/02/23 13:22:24 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 
 int main( void )
 {
-	Point a(-0.0f, -1.0f);
-	Point b(-0.0f, 1.1f);
-	Point c(4.0f, 0.0f);
-	Point point(0.5f, 0.5f);
+	Point a(-1.0f, -9.0f);
+	Point b(35.0f, 0.0f);
+	Point c(-1.0f, 9.0f);
 
-	Fixed x_min = Fixed::min(a.getX(), Fixed::min(b.getX(), c.getX())) - 1;
-	Fixed x_max = Fixed::max(a.getX(), Fixed::max(b.getX(), c.getX())) + 1;
+	Fixed x_min = Fixed::min(a.getX(), Fixed::min(b.getX(), c.getX())) - 5;
+	Fixed x_max = Fixed::max(a.getX(), Fixed::max(b.getX(), c.getX())) + 5;
 	Fixed y_min = Fixed::min(a.getY(), Fixed::min(b.getY(), c.getY())) - 1;
-	Fixed y_max = Fixed::max(a.getY(), Fixed::max(b.getY(), c.getY())) + 1;
+	Fixed y_max = Fixed::max(a.getY(), Fixed::max(b.getY(), c.getY())) + 2;
 
-	for (Fixed y = y_min; y < y_max; y = y + 0.1f)
+	for (Fixed y = y_min; y < y_max; y = y + 1.0f)
 	{
-		for (Fixed x = x_min; x < x_max; x = x + 0.1f)
+		std::cout << "    ";
+		for (Fixed x = x_min; x < x_max; x = x + 1.0f)
 		{
 			std::cout << (bsp(a, b, c, Point(x.toFloat(), y.toFloat())) ?  RESET : RED) << "█";
 		}
